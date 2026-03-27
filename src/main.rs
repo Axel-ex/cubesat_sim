14b7978724dee8306fa262bcdbe0edb7c0253902
Axel <axel.chabrerie@gmail.com>
2026-03-27 13:28:28 +0000

    dhde

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::time::Duration;
 
 use anyhow::Result;
+use cubesat_sim::state::SatState;
 use cubesat_sim::subsystems::coms::radio_task;
 use log::info;
 use tokio::time::sleep;
@@ -10,8 +11,10 @@ async fn main() -> Result<()> {
     env_logger::init();
 
     info!("Cubesat initialising...");
-    sleep(Duration::from_secs(1)).await;
     tokio::task::spawn(radio_task()).await??;
+
+    let mut state = SatState::new();
+    loop {}
 
     Ok(())
 }
---
+++ src/state.rs
@@ -0,0 +1,23 @@
+use serde::{Deserialize, Serialize};
+
+pub struct SatState {
+    mode: Mode,
+    reboot_count: u64,
+}
+
+#[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum Mode {
+    NOMINAL, //mission mode
+    DEGRADED,
+    SAFE,
+}
+
+impl SatState {
+    pub fn new() -> Self {
+        SatState {
+            mode: Mode::SAFE,
+            reboot_count: 0,
+        }
+    }
+}
